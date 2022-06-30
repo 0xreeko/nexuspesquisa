f0a1588329d8ae8a555169eadae67beffb5e2d29
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-06-30 15:44:21 +0100

    added footer

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+ 
+export const Footer = () => {
+    return (
+        <footer className={styles.container}>
+            <div className="">
+                
+            </div>
+        </footer>
+    )
+};
