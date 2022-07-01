c1b7c01ff815e96f67df5b9c6ab149ee10174f96
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-07-01 09:38:36 +0100

    tweaked svgs, and footer from moonstone to sylver

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,7 +32,7 @@ const Home: NextPage = () => {
       <form className={styles.form}>
         <Image src="https://cdn.freebiesupply.com/logos/thumbs/2x/google-nexus-logo.png" width={300} height={100} objectFit="cover" />
         <div className={styles.main}>
-        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
         <input type="text" name="search" id="search" className={styles.inputField} />
         </div>
         <div className={styles.groupedButtons}>
