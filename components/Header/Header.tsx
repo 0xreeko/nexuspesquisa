db55ed75ef66a9b03e7e05cd651304019809d7b7
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-07-01 10:45:53 +0100

    added link back to index on search page

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 import styles from './Header.module.css'
  
 export const Header = () => {
+    const router = useRouter()
     return (
         <header>
-            <Image src="https://cdn.freebiesupply.com/logos/thumbs/2x/google-nexus-logo.png" className={styles.logo} objectFit='cover' height={40} width={120}/>
+            <Image onClick={() => router.push('/')} src="https://cdn.freebiesupply.com/logos/thumbs/2x/google-nexus-logo.png" className={styles.logo} objectFit='cover' height={40} width={120}/>
         </header>
     )
 };
