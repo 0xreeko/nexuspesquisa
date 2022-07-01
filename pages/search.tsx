b2d985e8e647afaa97eb4bd338e957e15d4a4052
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-07-01 16:25:15 +0100

    added styles to pagination and more

--- pages/search.tsx
+++ pages/search.tsx
@@ -5,19 +5,17 @@ import { Header } from '../components/Header/Header';
 import { SearchResults } from '../components/SearchResults/SearchResults';
 import Response from '../Response';
 
-const Search: NextPage = ({results}: any) => {
+const Search: NextPage = ({ results }: any) => {
     const router = useRouter()
     console.log(results);
     return (
         <div className="h-full">
-        <Head>
-            <title>{router.query.term} - Nexus Search</title>
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
-        {/* header */}
-       <Header />
-        {/* search results */}
-        <SearchResults results={results}/>
+            <Head>
+                <title>{router.query.term} - Nexus Search</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Header />
+            <SearchResults results={results} />
         </div>
     )
 };
@@ -25,15 +23,11 @@ const Search: NextPage = ({results}: any) => {
 export default Search
 
 
-export async function getServerSideProps(ctx: { query: { term: any; start: any }; }) {
-    const useDummyData = true
+export async function getServerSideProps(ctx: any) {
+    const useDummyData = false
     const startIndex = ctx.query?.start || 0
-    const data = useDummyData ? Response : await fetch(`https://www.googleapis.com/customsearch/v1?key=
-    ${process.env.API_KEY}
-    &cx=${process.env.CTX_KEY}
-    &q=${ctx.query.term}
-    &start=${startIndex}`)
-    .then((res) => res.json())
+    const data = useDummyData ? Response : await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CTX_KEY}&q=${ctx.query.term}}`)
+        .then((res) => res.json())
 
     return {
         props: {
