1abc72d8bd4c10ec0d215e9fe6945c9eee094694
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-07-01 15:21:46 +0100

    added styles to Result elements

--- components/SearchResults/SearchResults.tsx
+++ components/SearchResults/SearchResults.tsx
@@ -6,10 +6,10 @@ export const SearchResults = ({results}: any) => {
             <p className={styles.about}>About {results.searchInformation.formattedTotalResults} results ({results.searchInformation.formattedSearchTime} seconds) </p>
             {results.items?.map((res: any) => (
                 <div className={styles.result} key={res.link}>
-                    <div>
+                    <div className='group'>
                         <a href={res.link} target="_blank" className={styles.link}>{res.formattedUrl}</a>
                         <a href={res.link} target="_blank">
-                            <h2 className={styles.title}>{res.title}</h2>
+                            <h2 className={`group-hover:underline ${styles.title}`}>{res.title}</h2>
                         </a>
                     </div>
                     <p className={styles.snippet}>{res.snippet}</p>
